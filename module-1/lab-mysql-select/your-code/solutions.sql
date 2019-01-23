2ffa80fcc434877fb68fee29c88dfb97cd6f41cd
Laura Repullo <laura_repullo@hotmail.com>
2019-01-23 19:33:14 +0100

    actualizacion 3 challenge

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -19,3 +19,15 @@ ON titles.title_id=titleauthor.title_id
 LEFT JOIN publishers
 ON titles.pub_id=publishers.pub_id
 GROUP BY authors.au_id, publishers.pub_name;
+
+#challenge 3
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', SUM(sales.qty) AS 'TOTAL'
+FROM authors
+INNER JOIN titleauthor
+ON titleauthor.au_id=authors.au_id
+INNER JOIN sales
+ON titleauthor.title_id=sales.title_id
+GROUP BY authors.au_id
+ORDER BY SUM(sales.qty) DESC
+LIMIT 3 ;
