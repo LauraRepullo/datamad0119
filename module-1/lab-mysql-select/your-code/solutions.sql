e12b4913999da7c5fdd9f9e6f020861fb13e7868
Laura Repullo <laura_repullo@hotmail.com>
2019-01-23 17:14:04 +0100

    challenge1

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -1,1 +1,7 @@
 #challenge 1
+
+SELECT authors.au_id AS 'AUTHOR ID', authors.au_lname AS 'LAST NAME', authors.au_fname AS 'FIRST NAME', titles.title AS TITLE, publishers.pub_name AS PUBLISHER
+FROM authors, titles, publishers
+WHERE title_id = title_id
+
+
