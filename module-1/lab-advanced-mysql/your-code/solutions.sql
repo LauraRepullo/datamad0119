afd7a982aebc095efdb6235f19b1d83b68e8aff0
Laura Repullo <laura_repullo@hotmail.com>
2019-01-27 11:51:04 +0100

    actualizacion step3

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -28,4 +28,19 @@ LEFT JOIN authors
 ON authors.au_id=titleauthor.au_id) summary
 group by title_id, au_id
 
+#STEP3_ no encuentro el fallo:
+
+SELECT summary.au_id, (SUM(SALES_ROYALTY) + titles.advance) AS profit
+FROM (SELECT titles.title_id,
+authors.au_id, 
+titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 AS 'SALES_ROYALTY' 
+FROM titles
+LEFT JOIN titleauthor ON titleauthor.title_id = titles.title_id
+LEFT JOIN sales ON sales.title_id = titles.title_id
+LEFT JOIN authors ON authors.au_id=titleauthor.au_id) summary
+LEFT JOIN titles on titles.title_id=summary.title_id
+GROUP BY au_id
+ORDER by profit DESC
+LIMIT 3
+
 
